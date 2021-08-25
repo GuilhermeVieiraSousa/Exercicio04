84b9a7a438af1a4f969ca60ff4089099b02385a7
Guilherme <guiilherme.v.sousa@hotmail.com>
2021-08-25 18:42:44 -0300

    Adicionando cursos e horarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercicio4';
+  textoBotao = "Esconder";  
+  esconder = false;  
+  
+  cursos = 
+  [    
+    { nome: "TypeScript", duracao: "12h" },    
+    { nome: "Java basico", duracao: "22h" }  
+  ];
+  
+  
+  alterarExibicao() {    
+    this.textoBotao = this.esconder ?  "Esconder" : "Exibir" ;    
+    this.esconder = !this.esconder;}
+
+    adicionar(nome, duracao) 
+    {    this.cursos = [{ nome: nome, duracao: duracao }, ...this.cursos];}
 }
